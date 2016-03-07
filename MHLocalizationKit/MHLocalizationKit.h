e5fd291d4b3a19de2b4091df5439f039824ec778
Milen Halachev <milenski88@gmail.com>
2016-03-07 23:16:28 +0200

    Initial Commit

---
+++ MHLocalizationKit/MHLocalizationKit.h
@@ -0,0 +1,19 @@
+//
+//  MHLocalizationKit.h
+//  MHLocalizationKit
+//
+//  Created by Milen Halachev on 3/7/16.
+//  Copyright © 2016 Milen Halachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHLocalizationKit.
+FOUNDATION_EXPORT double MHLocalizationKitVersionNumber;
+
+//! Project version string for MHLocalizationKit.
+FOUNDATION_EXPORT const unsigned char MHLocalizationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHLocalizationKit/PublicHeader.h>
+
+
