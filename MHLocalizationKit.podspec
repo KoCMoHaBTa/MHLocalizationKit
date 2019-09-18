a4203937fb30ccf02b86483af03646224f383190
Milen Halachev <milenski88@gmail.com>
2019-09-18 12:19:43 +0300

    - removed the convenience NSLocalizedStringFromTable and NSLocalizedStringWithDefaultValue functions because they break the updated genstrings, supplied with Xcode 11 - updated using Xcode 11 GM seed 2 - set version to 2.0

--- MHLocalizationKit.podspec
+++ MHLocalizationKit.podspec
@@ -1,14 +1,14 @@
 Pod::Spec.new do |s|
 
   s.name         = "MHLocalizationKit"
-  s.version      = "1.1.0"
+  s.version      = "2.0.0"
   s.source       = { :git => "https://github.com/KoCMoHaBTa/#{s.name}.git", :tag => "#{s.version}" }
   s.license      = { :type => "MIT", :file => "LICENSE" }
   s.author       = "Milen Halachev"
   s.summary      = "Changing languages while your iOS application is running."
   s.homepage     = "https://github.com/KoCMoHaBTa/#{s.name}"
 
-  s.swift_version = "5.0"
+  s.swift_version = "5.1"
   s.ios.deployment_target = "8.0"
 
   s.source_files  = "#{s.name}/**/*.swift", "#{s.name}/**/*.{h,m}"
--- MHLocalizationKit/MHLocalizationKit.swift
+++ MHLocalizationKit/MHLocalizationKit.swift
@@ -9,14 +9,3 @@
 import Foundation
 
 //https://developer.apple.com/library/ios/documentation/MacOSX/Conceptual/BPInternational/LanguageandLocaleIDs/LanguageandLocaleIDs.html
-
-//some convenience NSLocalizaedString functions that are missing from ObjC world and will allow genstrings to work properly
-public func NSLocalizedStringFromTable(_ key: String, _ tableName: String, _ comment: String) -> String {
-    
-    return NSLocalizedString(key, tableName: tableName, comment: comment)
-}
-
-public func NSLocalizedStringWithDefaultValue(_ key: String, _ tableName: String, _ bundle: Bundle, _ value: String, _ comment: String) -> String {
-    
-    return NSLocalizedString(key, tableName: tableName, bundle: bundle, value: value, comment: comment)
-}
